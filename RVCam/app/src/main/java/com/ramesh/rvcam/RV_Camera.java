9249c2df9ae20744dc8af82a3de1eacd79099b7c
Ramesh Viswanathan <rviswana@qti.qualcomm.com>
2015-07-23 23:12:54 -0700

    support to transfer image to pc

--- RVCam/app/src/main/java/com/ramesh/rvcam/RV_Camera.java
+++ RVCam/app/src/main/java/com/ramesh/rvcam/RV_Camera.java
@@ -2,7 +2,9 @@ package com.ramesh.rvcam;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
+import android.graphics.drawable.GradientDrawable;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -11,26 +13,54 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
 import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RV_Camera extends Activity {
+    private TCPServerThread testServer;
+    private Thread mServerThread;
+
     private TextureView mTextureView;
     private Size mPreviewSize;
     private CameraDevice mCamDevice;
     private CaptureRequest.Builder mPreviewBuilder;
     private CameraCaptureSession mPreviewSession;
+    private Button mBtnShot;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     protected void updatePreview() {
         if(null == mCamDevice) {
@@ -166,10 +196,25 @@ public class RV_Camera extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_rv__camera);
 
+        testServer = new TCPServerThread();
+        mServerThread = new Thread(testServer);
+        mServerThread.start();
+
         mTextureView = (TextureView) findViewById(R.id.previewTexture);
 
         //Set surface listener
         mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+
+        mBtnShot = (Button)findViewById(R.id.btn_takepicture);
+        mBtnShot.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.e("RV...", "mBtnShot clicked");
+                takePicture();
+            }
+        });
+
     }
 
     @Override
@@ -182,4 +227,146 @@ public class RV_Camera extends Activity {
             mCamDevice = null;
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("RV...", "onResume");
+    }
+
+     public void takePicture() {
+        if(null == mCamDevice){
+            Log.e("RV...", "Camera device is null");
+            return;
+        }
+
+        CameraManager camMgr = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try{
+            CameraCharacteristics  camCharacteristics = camMgr.getCameraCharacteristics(mCamDevice.getId());
+
+            Size[] jpegSizes = null;
+            if(camCharacteristics != null) {
+                jpegSizes = camCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+            }
+
+            int width = 640;
+            int height = 480;
+
+            if(jpegSizes != null && jpegSizes.length > 0) {
+                width = jpegSizes[0].getWidth();
+                height = jpegSizes[0].getHeight();
+            }
+
+            ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+
+            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
+            outputSurfaces.add(reader.getSurface());
+            outputSurfaces.add(new Surface(mTextureView.getSurfaceTexture()));
+
+
+            // get request builder
+            final CaptureRequest.Builder pictureBuilder = mCamDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+
+            // add reader target
+            pictureBuilder.addTarget(reader.getSurface());
+
+            // set builder property
+            pictureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            // set builder property - rotation
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            pictureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+
+
+            // file creation
+            final File file = new File(Environment.getExternalStorageDirectory()+"/DCIM", "rv_pic.jpg");
+
+            ImageReader.OnImageAvailableListener readListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image img = null;
+
+                    try {
+                       img = reader.acquireLatestImage();
+                        ByteBuffer buf = img.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buf.capacity()];
+                        buf.get(bytes);
+                        testServer.sendSnapshotResponse(4, bytes);
+                        save(bytes);
+                    }
+                    catch(FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+finally {
+                        if (img != null) {
+                            img.close();
+                        }
+                    }
+                }
+                private void save(byte[] bytes) throws IOException {
+                    OutputStream output = null;
+                    try {
+                        output = new FileOutputStream(file);
+                        output.write(bytes);
+                    } finally {
+                        if (null != output) {
+                            output.close();
+                        }
+                    }
+                }
+            };
+
+
+
+
+            HandlerThread thread = new HandlerThread("CameraPicture");
+            thread.start();
+            final Handler backgroudHandler = new Handler(thread.getLooper());
+            reader.setOnImageAvailableListener(readListener, backgroudHandler);
+
+            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
+
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session,
+                                               CaptureRequest request, TotalCaptureResult result) {
+
+                    super.onCaptureCompleted(session, request, result);
+                    Toast.makeText(RV_Camera.this, "Saved:"+file, Toast.LENGTH_SHORT).show();
+                    startPreview();
+                }
+
+            };
+
+            mCamDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+
+                    try {
+                        session.capture(pictureBuilder.build(), captureListener, backgroudHandler);
+                    } catch (CameraAccessException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, backgroudHandler);
+
+
+
+
+
+        } catch(CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
---
+++ RVCam/app/src/main/java/com/ramesh/rvcam/TCPServerThread.java
@@ -0,0 +1,217 @@
+package com.ramesh.rvcam;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TCPServerThread implements Runnable {
+    public static final int SERVER_PORT = 1456;
+    public RV_Camera mainClass;
+    private ServerSocket server;
+    private Socket socket;
+
+    private boolean isSendingDataReady = false;
+    private BufferedReader streamIn;
+    private DataInputStream streamIn_new;
+    private DataOutputStream streamOut;
+
+    private Deque<Byte> receiveBuffer = new ArrayDeque<Byte>(256);
+    private Deque<Byte> sendBuffer = new ArrayDeque<Byte>(256);
+
+    private boolean isReceivingPayload = false;
+    private int payloadId = 0;
+    private int payloadSize = 0;
+
+    public static final int HEADER_SIZE = 4;
+
+    private void sendEchoBack(int id) {
+        byte[] sendBuf = { 'P', 'I', 'N', 'G', ' ', 'A', 'C', 'K'};
+        sendResponse(sendBuf, id+1, sendBuf.length);
+    }
+
+    public void sendSnapshotResponse(int id, byte[] sendBuf) {
+        Log.e("RV..." , "id: "+ id);
+        Log.e("RV...", "length: "+ sendBuf.length);
+        //Log.e("RV...", sendBuf.toString());
+        sendResponse(sendBuf, id, sendBuf.length);
+    }
+
+    private void sendSnapshotResponse(int id) {
+        byte[] sendBuf = {1, 2, 3, 4, 5, 6};
+        sendResponse(sendBuf, id+1, sendBuf.length);
+    }
+
+    private void sendResponse(byte[] sendBuf, int id, int length)  {
+        for(int i =0; i < sendBuf.length; ++i) {
+            sendBuffer.push(sendBuf[length - i -1]);
+        }
+
+        byte idByteArr[] = BigInteger.valueOf(id).toByteArray();
+        for(int i = 0; i < 4 - idByteArr.length; ++i) {
+            sendBuffer.push((byte)0);
+        }
+
+        for(int i = 0; i < idByteArr.length; ++i) {
+            sendBuffer.push(idByteArr[i]);
+        }
+
+        byte lengthByteArr[] = BigInteger.valueOf(length).toByteArray();
+        //Log.e("RV...", "lengthByteArr " + lengthByteArr.length);
+
+        for(int i = 0; i < 4 - lengthByteArr.length; ++i) {
+            sendBuffer.push((byte)0);
+        }
+
+        for(int i = 0; i < lengthByteArr.length; ++i) {
+            sendBuffer.push(lengthByteArr[i]);
+        }
+        isSendingDataReady = true;
+    }
+
+    public void parseReceivedPacket() {
+
+        if(isReceivingPayload == false) {
+            if(receiveBuffer.size() >= 2 * HEADER_SIZE ){
+                byte[] length = new byte[HEADER_SIZE];
+                for(int i=0; i < HEADER_SIZE; i++) {
+                    length[i] = receiveBuffer.pop();
+                }
+                ByteBuffer buffer = ByteBuffer.wrap(length);
+                buffer.order(ByteOrder.LITTLE_ENDIAN);
+                payloadSize = buffer.getInt();
+
+                byte[] length1 = new byte[HEADER_SIZE];
+                for(int i=0; i < HEADER_SIZE; i++) {
+                    length1[i] = receiveBuffer.pop();
+                }
+                ByteBuffer buffer1 = ByteBuffer.wrap(length1);
+                buffer1.order(ByteOrder.LITTLE_ENDIAN);
+                payloadId = buffer1.getInt();
+
+                isReceivingPayload = true;
+            }
+        }
+
+        if(isReceivingPayload == true) {
+            if(receiveBuffer.size() >= payloadSize) {
+                byte[] payload = new byte[payloadSize];
+                for(int i=0; i < payloadSize; i++) {
+                    payload[payloadSize - i -1] = receiveBuffer.pop();
+                }
+                if(payloadId == 1) {
+                    sendEchoBack(payloadId);
+                }
+                else if(payloadId == 3) {
+                   mainClass.takePicture();
+                    Log.e("RV....", "statrring takePicture");
+                    //sendSnapshotResponse(payloadId);
+                }
+            }
+            isReceivingPayload = false;
+        }
+    }
+
+    public void run() {
+        try {
+            boolean done = false;
+
+            openSocket();
+            openStream();
+
+            while (done == false) {
+                try {
+                    byte[] line = new byte[256];
+                    if(streamIn_new.available() >0) {
+                        int readData = streamIn_new.read(line, 0, 256);
+
+                        Log.e("RV....", "inside run loop2");
+
+                        if (readData > 0) {
+                            for (int i = 1; i <= readData; ++i) {
+                                receiveBuffer.push(line[readData - i]);
+                            }
+                            Log.e("RV....:", "received data" + readData);
+                        }
+                        //Log.e("RV....", "inside loop");
+                        Log.e("RV....", "inside run loop3");
+                        while (receiveBuffer.isEmpty() == false) {
+                            parseReceivedPacket();
+                        }
+                    }
+                    if(isSendingDataReady == true) {
+                        if (sendBuffer.isEmpty() != true) {
+                            //Log.e("RV...", "in sendBuffer: " + sendBuffer.size());
+                            int sendSize = sendBuffer.size();
+                            byte[] sendBuf = new byte[sendSize];
+
+                            for (int i = 0; i < sendSize; ++i) {
+                                sendBuf[i] = sendBuffer.pop();
+                            }
+                            streamOut.write(sendBuf, 0, sendSize);
+                            Log.e("RV...", "sent data: " + sendSize);
+
+                        }
+                        isSendingDataReady = false;
+                    }
+                } catch (IOException ioe) {
+                    Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
+                    done = true;
+                }
+            }
+            closeStream();
+        } catch (IOException ioe) {
+            Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
+        }
+        finally {
+            try {
+                closeStream();
+                closeSocket();
+            } catch(IOException ioe) {
+                Log.e("RV..EXCEPT.....", ioe.getLocalizedMessage());
+            }
+        }
+    }
+
+    private void openSocket() throws IOException {
+        System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
+        server = new ServerSocket(SERVER_PORT);
+        Log.e("RV..............", "Server started: " + server);
+
+        Log.e("RV............", "Waiting for a client ...");
+        socket = server.accept();
+        Log.e("RV............", ("Client accepted: " + socket));
+    }
+
+    private void openStream() throws IOException {
+        //streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        streamIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        streamOut = new DataOutputStream(socket.getOutputStream());
+        streamIn_new = new DataInputStream(socket.getInputStream());
+    }
+
+    private void closeSocket() throws IOException {
+        if (socket != null)
+            socket.close();
+    }
+
+    private void closeStream() throws IOException {
+        if (streamIn != null)
+            streamIn.close();
+        if (streamIn_new != null)
+            streamIn_new.close();
+
+        if (streamOut != null)
+            streamOut.close();
+    }
+}
